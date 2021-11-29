1b617a2ea72dfdeda72790b4eec027000cdd0761
yuanwang <yuan.wang@workiva.com>
2021-11-29 08:25:42 -0600

    use devshell

--- haskell-hello/flake.nix
+++ haskell-hello/flake.nix
@@ -1,35 +1,36 @@
 {
   description = "virtual environments";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, flake-utils, devshell, nixpkgs }:
+  outputs = { self, nixpkgs, devshell, flake-utils }:
 
-    let out = system:
-          let pkgs = import nixpkgs {
-                inherit system;
-                overlays = [devshell.overlay self.overlap]  ;
-              };
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlay
+            (final: prev: {
+              haskell-hello =
+                final.haskellPackages.callCabal2nix "haskell-hello" ./. { };
+            })
+          ];
+        };
+        myHaskellEnv = (pkgs.haskellPackages.ghcWithHoogle (p:
+              with p;
+              [ cabal-install ormolu hlint hpack brittany ]
+           ));
 
-                 myHaskellEnv = (pkgs.haskellPackages.ghcWithHoogle (p:
-          with p;
-          [ haskell-hello cabal-install ghcid  ]
-
-          ++ pkgs.haskell-hello.buildInputs));
-
-          in {
-
-            overlay = (final: prev: {
-        haskell-hello = final.haskellPackages.callCabal2nix "haskell-hello" ./. {};
+      in {
+        packages = { haskell-hello = pkgs.haskell-hello; };
+        defaultPackage = pkgs.haskell-hello;
+        checks = self.packages;
+        devShell = pkgs.devshell.mkShell {
+          name = "hello";
+          packages = [myHaskellEnv];
+        };
       });
-            defaultPackage = pkgs.haskell-hello;
-            devShell = pkgs.devshell.mkShell {
-               packages = [ myHaskellEnv pkgs.nixfmt pkgs.treefmt ];
-            };
-          };
-
-
-    in with flake-utils.lib;
-      eachDefaultSystem out;
 }
