80ee4c423e9e4b86a709d995637ab60735050f4d
yuanwang <yuan.wang@workiva.com>
2022-06-20 14:49:55 -0600

    update

--- servant-halogen/flake.nix
+++ servant-halogen/flake.nix
@@ -2,13 +2,14 @@
   description = "A Hello World in Haskell with a dependency and a devShell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
- easy-ps-nix = {
+    easy-ps-nix = {
       url = "github:justinwoo/easy-purescript-nix/master";
       flake = false;
-    };  };
+    };
+  };
 
   outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, easy-ps-nix }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -17,14 +18,14 @@
           inherit system;
           overlays = [ ];
         };
- easy-ps = import easy-ps-nix { inherit pkgs; };
-  inherit (easy-ps) psa purescript-language-server purs purs-tidy spago spago2nix;
+        easy-ps = import easy-ps-nix { inherit pkgs; };
+        inherit (easy-ps) psa purescript-language-server purs purs-tidy spago spago2nix;
 
         t = pkgs.lib.trivial;
         hl = pkgs.haskell.lib;
 
         name = "servant-halogen-hello";
-    purs-tidy-hook = {
+        purs-tidy-hook = {
           enable = true;
           name = "purs-tidy";
           entry = "${purs-tidy}/bin/purs-tidy format-in-place";
@@ -79,13 +80,12 @@
           haskell-language-server
           hlint
           ormolu
-          psa
 
-            purescript-language-server
-            purs
-            purs-tidy
-            spago
-            spago2nix
+          purescript-language-server
+          purs
+          purs-tidy
+          spago
+          spago2nix
         ]);
 
       });
