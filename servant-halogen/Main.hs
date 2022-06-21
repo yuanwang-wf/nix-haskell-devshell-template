0825a14046c6280508b1be705fbb7428d7462734
yuanwang <yuan.wang@workiva.com>
2022-06-21 11:08:59 -0600

    update

--- servant-halogen/Main.hs
+++ servant-halogen/Main.hs
@@ -1,24 +1,32 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
 import Data.Aeson
+import Data.Functor ((<$>))
+import Data.Maybe (fromMaybe)
 import Data.Proxy
 import Data.Text (Text)
 import qualified Data.Text as T
 import GHC.Generics
 import qualified Lucid as L
 import Lucid.Base
-import Network.HTTP.Types hiding (Header)
 import Network.Wai
 import Network.Wai.Application.Static
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.Gzip
 import Network.Wai.Middleware.RequestLogger
+import RIO
 import Servant
 import Servant.Server.Internal
+import System.Environment (lookupEnv)
 import qualified System.IO as IO
 
-main :: IO ()
+main :: IO.IO ()
 main = do
+  port <- fmap (fromMaybe 8080 . join . fmap readMaybe) $ lookupEnv "PORT"
+  staticFilePath <- fromMaybe "/var/www" <$> lookupEnv "STATIC_FILE_PATH"
   IO.hPutStrLn IO.stderr "Running on port 3002..."
   run 3002 $ logStdout (compress app)
   where
@@ -27,7 +35,10 @@ main = do
 -- | API type
 type API = ("static" :> Raw)
 
+myAPI :: Proxy API
+myAPI = Proxy
+
 app :: Application
-app = serve (Proxy @ API) (static)
+app = serve myAPI (static)
   where
     static = serveDirectoryWith (defaultWebAppSettings "static")
