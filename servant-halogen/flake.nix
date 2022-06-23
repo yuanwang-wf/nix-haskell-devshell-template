e2486b993049b042921378ac223479d0d403cdef
yuanwang <yuan.wang@workiva.com>
2022-06-23 11:33:37 -0600

    does this build

--- servant-halogen/flake.nix
+++ servant-halogen/flake.nix
@@ -18,8 +18,8 @@
           spago = prev.spago.overrideAttrs (o: rec {
             src = prev.fetchgit {
               url = "https://github.com/yuanwang-wf/spago.git";
-              sha256 = "00vdqg7vaw3d9zwh47886lw9fhhlwjagzhaj3aqz4xm92pjavhih";
-              rev = "d16d4914200783fbd820ba89dbdf67270454faf5";
+              sha256 = "10wy61zc236jzpngmw1qplcbfkjcjdk2aifzkbm697gqpks2vskd";
+              rev = "5d195e13cecdd2a3a91f3bca687f54e71ba6e463";
               fetchSubmodules = true;
             };
           });
